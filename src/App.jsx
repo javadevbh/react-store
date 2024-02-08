1d3ceaff76ee35efe326ffc9afcab682d8b4fc9b
Javad Bahrami <javadev14bh@gmail.com>
2024-02-08 15:06:47 +0330

    Install react router dom and add project routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+//Pages
+import CheckoutPage from "./pages/CheckoutPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import ProductsPage from "./pages/ProductsPage";
+import PageNotFound from "./pages/404";
+
 function App() {
   return (
     <>
-      <div>App</div>
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
